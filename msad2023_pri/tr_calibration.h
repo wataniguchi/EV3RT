77bc8ae839566cf5b08ca620b6cda0cc63ab6156
Wataru Taniguchi <wataru@taniguchifamily.com>
2023-07-13 17:15:54 +0900

    arm reset defect fixed

--- msad2023_pri/tr_calibration.h
+++ msad2023_pri/tr_calibration.h
@@ -1,8 +1,8 @@
 #define TR_CALIBRATION \
   .composite<BrainTree::MemSequence>() \
     .leaf<IsTimeEarned>(1000000) /* wait one sec for filling the filter pipeline */ \
-    .leaf<SetArmPosition>(-15,30) /* lift up the arm... */ \
-    .leaf<ResetArm>() /* and mark arm position as zero */ \
+    .leaf<ResetArm>() /* reset arm */ \
+    .leaf<SetArmPosition>(30,30) /* then down */ \
     /* wait until Touch Button is pressed */ \
   /*.decorator<BrainTree::UntilSuccess>() \
       .leaf<IsTouchOn>() \
