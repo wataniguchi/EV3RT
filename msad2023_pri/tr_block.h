2d8e8f9517c255d46364eeeff58927b064a319b4
Wataru Taniguchi <wataru@taniguchifamily.com>
2023-07-13 23:44:20 +0900

    arm handling completed

--- msad2023_pri/tr_block.h
+++ msad2023_pri/tr_block.h
@@ -2,4 +2,14 @@
   .leaf<StopNow>()
 
 #define TR_BLOCK_L \
-  .leaf<StopNow>()
+  /* BLOCK_L: make a turn, down the arm, and capture Treasure Block */ \
+  .composite<BrainTree::MemSequence>() \
+    /* section L1: rotate and arm down */ \
+    .composite<BrainTree::ParallelSequence>(1,2) \
+      .leaf<IsTimeEarned>(prof->getValueAsNum("BLOCK_L1_TIME")) \
+      .leaf<RunAsInstructed>(prof->getValueAsNum("BLOCK_L1_PWML"), \
+	  prof->getValueAsNum("BLOCK_L1_PWMR"), 0.0) \
+    .end() \
+    .leaf<StopNow>() \
+    .leaf<ArmUpDownFull>(AD_DOWN) \
+  .end()
