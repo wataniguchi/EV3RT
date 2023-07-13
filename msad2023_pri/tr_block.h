32f83b139a2a5b9159470393bfb44d36673e524d
Wataru Taniguchi <wataru@taniguchifamily.com>
2023-07-13 15:55:16 +0900

    snap

--- msad2023_pri/tr_block.h
+++ msad2023_pri/tr_block.h
@@ -4,9 +4,13 @@
 #define TR_BLOCK_L \
   /* BLOCK_L: make a turn, down the arm, and capture Treasure Block */ \
   .composite<BrainTree::MemSequence>() \
-    .leaf<RotateEV3>(prof->getValueAsNum("BLOCK_L1_ROTDEG"), \
-	  prof->getValueAsNum("BLOCK_L1_SPEED"), 0.0) \
-    .leaf<SetArmPosition>(prof->getValueAsNum("BLOCK_L1_ARMDEG"), \
-	  prof->getValueAsNum("BLOCK_L1_ARMRPM")) \
+    /* section L1: rotate and arm down */ \
+    .composite<BrainTree::ParallelSequence>(1,2) \
+      .leaf<IsTimeEarned>(prof->getValueAsNum("BLOCK_L1_TIME")) \
+      .leaf<RunAsInstructed>(prof->getValueAsNum("BLOCK_L1_PWML"), \
+	  prof->getValueAsNum("BLOCK_L1_PWMR"), 0.0) \
+    .end() \
+    .leaf<SetArmPosition>(prof->getValueAsNum("BLOCK_L1_ARMDEG"),	\
+	  prof->getValueAsNum("BLOCK_L1_PWMARM")) \
     .leaf<StopNow>() \
   .end()
