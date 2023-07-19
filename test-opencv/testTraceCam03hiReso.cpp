c04141a262ec9ea702fefd9435deedbe371365cf
Wataru Taniguchi <wataru@taniguchifamily.com>
2023-07-19 10:54:05 +0900

    testTraceCam03hiReso.cpp added

---
+++ test-opencv/testTraceCam03hiReso.cpp
@@ -0,0 +1,279 @@
+/*
+  how to compile:
+
+  g++ testTraceCam03hiReso.cpp -std=c++14 `pkg-config --cflags --libs opencv4` -I../msad2023_pri -I/opt/raspivideocap/include -lraspivideocap -L/opt/raspivideocap/lib -o testTraceCam03hiReso
+*/
+
+/* 
+  NppCpp by David Pilger
+  git clone https://github.com/dpilger26/NumCpp.git
+  the use of NumCpp requires -std=c++14 -I . for compilation
+*/
+#include "NumCpp.hpp"
+
+#include <iostream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/utils/logger.hpp>
+#include <thread>
+#include <cmath>
+
+#include <vector>
+#include <chrono>
+/*
+  raspivideocap by coyote009@github modified for the use with OpenCV4
+  git clone https://github.com/wataniguchi/raspivideocap.git
+*/
+#include <raspivideocap.h>
+
+using namespace std;
+using namespace cv;
+using std::this_thread::sleep_for;
+
+/* frame size for Raspberry Pi camera capture */
+#define IN_FRAME_WIDTH  1640
+#define IN_FRAME_HEIGHT 1232
+#define IN_FPS 40
+
+/* frame size for OpenCV */
+//#define FRAME_WIDTH  640
+//#define FRAME_HEIGHT 480
+#define FRAME_WIDTH  160
+#define FRAME_HEIGHT 120
+
+#define ROI_BOUNDARY int(FRAME_WIDTH/16)
+#define LINE_THICKNESS int(FRAME_WIDTH/80)
+#define CIRCLE_RADIUS int(FRAME_WIDTH/40)
+
+/* frame size for X11 painting */
+#define OUT_FRAME_WIDTH  160
+#define OUT_FRAME_HEIGHT 120
+
+int gs_min=0,gs_max=100,edge=0,algo=0,gs_block=50,gs_C=50;
+
+enum BinarizationAlgorithm {
+  BA_NORMAL = 0,
+  BA_ADAPTIVE = 1,
+  BA_OTSU = 2,
+};
+
+static char algoName[3][25] = {"normal binarization", "adaptive binarization", "Otsu's binarization"};
+
+int main() {
+  utils::logging::setLogLevel(utils::logging::LOG_LEVEL_WARNING);
+  /* set number of threads */
+  setNumThreads(0);
+  /* prepare the camera */
+  RaspiVideoCapture cap(0);
+  int inFrameHeight = 16 * ceil(IN_FRAME_HEIGHT/16);
+  int inFrameWidth  = 32 * ceil(IN_FRAME_WIDTH /32);
+  if (!cap.open(inFrameWidth, inFrameHeight, IN_FPS)) {
+    cout << "cap is not open" << endl;
+  }
+
+  /* create trackbars */
+  namedWindow("testTrace1");
+  createTrackbar("GS_min", "testTrace1", nullptr, 255, nullptr);
+  setTrackbarPos("GS_min", "testTrace1", gs_min);
+  createTrackbar("GS_max", "testTrace1", nullptr, 255, nullptr);
+  setTrackbarPos("GS_max", "testTrace1", gs_max);
+  createTrackbar("Edge",   "testTrace1", nullptr, 2, nullptr);
+  setTrackbarPos("Edge",   "testTrace1", edge);
+  createTrackbar("Bin Algorithm",   "testTrace1", nullptr, 2, nullptr);
+  setTrackbarPos("Bin Algorithm",   "testTrace1", algo);
+  createTrackbar("GS_block (adaptive)", "testTrace1", nullptr, FRAME_WIDTH, nullptr);
+  setTrackbarPos("GS_block (adaptive)", "testTrace1", gs_block);
+  createTrackbar("GS_C (adaptive)", "testTrace1", nullptr, 255, nullptr);
+  setTrackbarPos("GS_C (adaptive)", "testTrace1", gs_C);
+
+  /* initial region of interest */
+  Rect roi(0, 0, FRAME_WIDTH, FRAME_HEIGHT);
+  /* initial trace target */
+  int mx = (int)(FRAME_WIDTH/2);
+
+  std::vector<std::uint32_t> read_elaps, upd_elaps;
+  
+  while (true) {
+    /* obtain values from the trackbars */
+    gs_min = getTrackbarPos("GS_min", "testTrace1");
+    gs_max = getTrackbarPos("GS_max", "testTrace1");
+    edge   = getTrackbarPos("Edge",   "testTrace1");
+    algo   = getTrackbarPos("Bin Algorithm",   "testTrace1");
+    gs_block = getTrackbarPos("GS_block (adaptive)",   "testTrace1");
+    gs_C   = getTrackbarPos("GS_C (adaptive)",   "testTrace1");
+
+    Mat frame, img_orig, img_gray, img_gray_part, img_bin_part, img_bin, img_bin_mor;
+    int c;
+
+    sleep_for(chrono::milliseconds(10));
+    
+    std::chrono::system_clock::time_point ts_upd  = std::chrono::system_clock::now();
+    cap.read(frame);
+    std::chrono::system_clock::time_point te_read = std::chrono::system_clock::now();
+    read_elaps.push_back(std::chrono::duration_cast<std::chrono::microseconds>(te_read - ts_upd).count());
+
+    /* clone the frame if exists, otherwise use the previous image */
+    if (!frame.empty()) {
+      img_orig = frame.clone();
+    }
+    /* resize the image for OpenCV processing */
+    if (FRAME_WIDTH != IN_FRAME_WIDTH || FRAME_HEIGHT != IN_FRAME_HEIGHT) {
+      Mat img_resized;
+      resize(img_orig, img_resized, Size(), (double)FRAME_WIDTH/inFrameWidth, (double)FRAME_HEIGHT/inFrameHeight);
+      assert(img_resized.size().width == FRAME_WIDTH);
+      assert(img_resized.size().height == FRAME_HEIGHT);
+      img_orig = img_resized;
+    }
+    /* convert the image from BGR to grayscale */
+    cvtColor(img_orig, img_gray, COLOR_BGR2GRAY);
+    /* crop bottom two thirds vertically and center half horizontally */
+    int ubound = static_cast<int>(FRAME_HEIGHT/3);
+    int lbound = static_cast<int>(FRAME_WIDTH/4);
+    img_gray_part = img_gray(Range(ubound,FRAME_HEIGHT), Range(lbound,FRAME_WIDTH-lbound));
+    /* binarize the image */
+    switch (algo) {
+      case BA_NORMAL:
+        inRange(img_gray_part, gs_min, gs_max, img_bin_part);
+	break;
+      case BA_ADAPTIVE:
+	gs_block = 2 * ceil((gs_block - 1) / 2) + 1;
+	if (gs_block < 3) { gs_block = 3; }
+	adaptiveThreshold(img_gray_part, img_bin_part, gs_max, ADAPTIVE_THRESH_MEAN_C, THRESH_BINARY_INV, gs_block, gs_C); 
+	break;
+      case BA_OTSU:
+	threshold(img_gray_part, img_bin_part, gs_max, 255, THRESH_BINARY_INV+THRESH_OTSU); 
+	break;
+      default:
+	break;
+    }
+    /* prepare an empty matrix */
+    img_bin = Mat::zeros(FRAME_HEIGHT, FRAME_WIDTH, CV_8UC1);
+    /* copy img_bin_part into img_bin */
+    for (int i = ubound; i < FRAME_HEIGHT; i++) {
+      for (int j = lbound; j < FRAME_WIDTH-lbound; j++) {
+        img_bin.at<uchar>(i,j) = img_bin_part.at<uchar>(i-ubound,j-lbound); /* type = CV_8U */
+    	}
+    }
+    /* remove noise */
+    Mat kernel = Mat::zeros(Size(7,7), CV_8UC1);
+    morphologyEx(img_bin, img_bin_mor, MORPH_CLOSE, kernel);
+
+    /* focus on the region of interest */
+    Mat img_roi(img_bin_mor, roi);
+    /* find contours in the roi with offset */
+    vector<vector<Point>> contours;
+    vector<Vec4i> hierarchy;
+    findContours(img_roi, contours, hierarchy, RETR_EXTERNAL, CHAIN_APPROX_SIMPLE, Point(roi.x,roi.y));
+    /* identify the largest contour */
+    if (contours.size() >= 1) {
+      int i_area_max = 0;
+      double area_max = 0.0;
+      for (int i = 0; i < contours.size(); i++) {
+	double area = contourArea(contours[i]);
+	if (area > area_max) {
+	  area_max = area;
+	  i_area_max = i;
+	}
+      }
+      /* draw the largest contour on the original image */
+      //drawContours(img_orig, (vector<vector<Point>>){contours[i_area_max]}, 0, Scalar(0,255,0), LINE_THICKNESS);
+      polylines(img_orig, (vector<vector<Point>>){contours[i_area_max]}, 0, Scalar(0,255,0), LINE_THICKNESS);
+
+      /* calculate the bounding box around the largest contour
+	 and set it as the new region of interest */ 
+      roi = boundingRect(contours[i_area_max]);
+      /* adjust the region of interest */
+      roi.x = roi.x - ROI_BOUNDARY;
+      roi.y = roi.y - ROI_BOUNDARY;
+      roi.width = roi.width + 2*ROI_BOUNDARY;
+      roi.height = roi.height + 2*ROI_BOUNDARY;
+      if (roi.x < 0) {
+	roi.x = 0;
+      }
+      if (roi.y < 0) {
+	roi.y = 0;
+      }
+      if (roi.x + roi.width > FRAME_WIDTH) {
+	roi.width = FRAME_WIDTH - roi.x;
+      }
+      if (roi.y + roi.height > FRAME_HEIGHT) {
+	roi.height = FRAME_HEIGHT - roi.y;
+      }
+ 
+      /* prepare for trace target calculation */
+      Mat img_cnt = Mat::zeros(img_orig.size(), CV_8UC3);
+      drawContours(img_cnt, (vector<vector<Point>>){contours[i_area_max]}, 0, Scalar(0,255,0), 1);
+      Mat img_cnt_gray;
+      cvtColor(img_cnt, img_cnt_gray, COLOR_BGR2GRAY);
+      /* scan the line really close to the image bottom to find edges */
+      Mat scan_line = img_cnt_gray.row(img_cnt_gray.size().height - LINE_THICKNESS);
+      /* convert the Mat to a NumCpp array */
+      auto scan_line_nc = nc::NdArray<nc::uint8>(scan_line.data, scan_line.rows, scan_line.cols);
+      auto edges = scan_line_nc.flatnonzero();
+      if (edges.size() >= 2) {
+	if (edge == 0) {
+	  mx = edges[0];
+	} else if (edge == 1) {
+	  mx = edges[edges.size()-1];
+	} else {
+	  mx = (int)((edges[0]+edges[edges.size()-1]) / 2);
+	}
+      } else if (edges.size() == 1) {
+	mx = edges[0];
+      }
+    } else { /* contours.size() == 0 */
+      roi = Rect(0, 0, FRAME_WIDTH, FRAME_HEIGHT);
+    }
+
+    /* draw the area of interest on the original image */
+    rectangle(img_orig, Point(roi.x,roi.y), Point(roi.x+roi.width,roi.y+roi.height), Scalar(255,0,0), LINE_THICKNESS);
+    /* draw the trace target on the image */
+    circle(img_orig, Point(mx, FRAME_HEIGHT-LINE_THICKNESS), CIRCLE_RADIUS, Scalar(0,0,255), -1);
+    /* calculate variance of mx from the center in pixel */
+    int vxp = mx - (int)(FRAME_WIDTH/2);
+    /* convert the variance from pixel to milimeters
+       72 is length of the closest horizontal line on ground within the camera vision */
+    float vxm = vxp * 72 / FRAME_WIDTH;
+    /* calculate the rotation in radians (z-axis)
+       284 is distance from axle to the closest horizontal line on ground the camera can see */
+    float theta = atan(vxm / 284);
+    cout << "mx = " << mx << ", vxm = " << vxm << ", theta = " << theta << ", " << (char*)algoName[algo] << endl;
+
+    /* shrink the image to avoid delay in transmission */
+    if (OUT_FRAME_WIDTH != FRAME_WIDTH || OUT_FRAME_HEIGHT != FRAME_HEIGHT) {
+      Mat img_resized;
+      resize(img_orig, img_resized, Size(), (double)OUT_FRAME_WIDTH/FRAME_WIDTH, (double)OUT_FRAME_HEIGHT/FRAME_HEIGHT);
+      img_orig = img_resized;
+    }
+
+    std::chrono::system_clock::time_point te_upd = std::chrono::system_clock::now();
+    upd_elaps.push_back(std::chrono::duration_cast<std::chrono::microseconds>(te_upd - ts_upd).count());
+    
+    /* transmit and display the image */
+    imshow("testTrace2", img_orig);
+
+    c = waitKey(1);
+    if ( c == 'q' || c == 'Q' ) break;
+  }
+
+  size_t exec_count = read_elaps.size();
+  size_t median_index = exec_count / 2;
+  std::sort(std::begin(read_elaps), std::end(read_elaps));
+  int read_median = (exec_count % 2 == 0
+		? static_cast<int>(read_elaps[median_index] + read_elaps[median_index - 1]) / 2
+		: read_elaps[median_index]);
+  printf("frame read elaps (micro sec): max = %d, min = %d, mean = %d, median = %d\n",
+	 (int)(*std::max_element(std::begin(read_elaps),std::end(read_elaps))),
+	 (int)(*std::min_element(std::begin(read_elaps),std::end(read_elaps))),
+	 (int)(std::accumulate(std::begin(read_elaps),std::end(read_elaps),0) / exec_count), read_median);
+  std::sort(std::begin(upd_elaps), std::end(upd_elaps));
+  int upd_median = (exec_count % 2 == 0
+		? static_cast<int>(upd_elaps[median_index] + upd_elaps[median_index - 1]) / 2
+		: upd_elaps[median_index]);
+  printf("update process elaps (micro sec): max = %d, min = %d, mean = %d, median = %d\n",
+	 (int)(*std::max_element(std::begin(upd_elaps),std::end(upd_elaps))),
+	 (int)(*std::min_element(std::begin(upd_elaps),std::end(upd_elaps))),
+	 (int)(std::accumulate(std::begin(upd_elaps),std::end(upd_elaps),0) / exec_count), upd_median);
+
+  destroyAllWindows();
+  return 0;
+}
