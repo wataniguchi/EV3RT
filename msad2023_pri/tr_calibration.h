a8ef5d41f04693c60ce62dc24992d0a10f1d09e2
miki.aosa <miki.aosa@gmail.com>
2023-09-07 18:02:34 +0900

    blocktest20230907

--- msad2023_pri/tr_calibration.h
+++ msad2023_pri/tr_calibration.h
@@ -4,9 +4,10 @@
     .leaf<ArmUpDownFull>(AD_UP) /* raise arm to the full */ \
     .leaf<ResetArm>() /* reset arm angle to zero */ \
     .leaf<SetArmPosition>(prof->getValueAsNum("CAL_x_ARM_ANGLE"),ARM_SHIFT_PWM) /* then down */ \
+    .leaf<SetMotorAdjustmentFactors>(prof->getValueAsNumVec("CAL_x_MOTOR_ADJ_FACTORS")) \
     /* wait until Touch Button is pressed */ \
-    .decorator<BrainTree::UntilSuccess>()  \
+    /*.decorator<BrainTree::UntilSuccess>()  \
       .leaf<IsEnterOn>() \
-      .end()		 \
+      .end() */		 \
     .leaf<ResetClock>()	\
   .end()
