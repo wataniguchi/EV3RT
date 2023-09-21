db33fee5ecbfc739a0f9d83426c99932d8fded33
3939kunkun <kumiko_yukari_ryosuke@yahoo.co.jp>
2023-09-22 01:06:31 +0900

    9/22 Ｒコース修正

--- msad2023_ent/tr_run.h
+++ msad2023_ent/tr_run.h
@@ -5,28 +5,28 @@
             .composite<BrainTree::MemSequence>() \
                 .composite<BrainTree::ParallelSequence>(1,2) \
                     .leaf<IsTimeEarned>(7200000) \
-                    .leaf<RunAsInstructed>(90, 90, 0.5) \
+                    .leaf<RunAsInstructed>(100, 100, 0.5) \
                 .end() \
                 .composite<BrainTree::ParallelSequence>(1,2) \
                     .leaf<IsTimeEarned>(1800000) \
-                    .leaf<RunAsInstructed>(90, 65, 0.0) \
+                    .leaf<RunAsInstructed>(100, 65, 0.0) \
                 .end() \
                 .composite<BrainTree::ParallelSequence>(1,2) \
                     .leaf<IsTimeEarned>(1200000) \
-                    .leaf<RunAsInstructed>(90, 73, 0.0) \
+                    .leaf<RunAsInstructed>(100, 73, 0.0) \
                 .end() \
                 .composite<BrainTree::ParallelSequence>(1,2) \
-                    .leaf<IsTimeEarned>(2000000) \
-                    .leaf<RunAsInstructed>(90, 90, 0.0) \
+                    .leaf<IsTimeEarned>(1700000) \
+                    .leaf<RunAsInstructed>(100, 100, 0.0) \
                 .end() \                
                 .composite<BrainTree::ParallelSequence>(1,2) \
                     .leaf<IsTimeEarned>(700000) \
-                    .leaf<RunAsInstructed>(75, 90, 0.0) \
+                    .leaf<RunAsInstructed>(78, 100, 0.0) \
                 .end() \  
                 .composite<BrainTree::ParallelSequence>(1,2) \
-                    .leaf<IsTimeEarned>(1500000) \
-                    .leaf<RunAsInstructed>(90, 45, 0.5) \
-                .end() \                  
+                    .leaf<IsTimeEarned>(1200000) \
+                    .leaf<RunAsInstructed>(100, 45, 0.5) \
+                .end() \                              
                 .composite<BrainTree::ParallelSequence>(1,2) \
                     .leaf<IsColorDetected>(CL_BLUE) \
                     .leaf<TraceLine>(77, GS_TARGET, P_CONST, I_CONST, D_CONST, 0.0, TS_NORMAL) \
