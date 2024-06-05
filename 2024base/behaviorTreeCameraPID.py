facdba10881a886a582a2f524ab699ea80f4df07
Wataru Taniguchi <wataru@taniguchifamily.com>
2024-06-05 16:27:23 +0900

    video trace initial version

---
+++ 2024base/behaviorTreeCameraPID.py
@@ -0,0 +1,323 @@
+import argparse
+import time
+import math
+import threading
+import signal
+from etrobo_python import ColorSensor, ETRobo, Hub, Motor, TouchSensor
+from simple_pid import PID
+import py_trees.common
+from py_trees.trees import BehaviourTree
+from py_trees.behaviour import Behaviour
+from py_trees.common import Status
+from py_trees.composites import Sequence
+from py_trees.composites import Parallel
+from py_trees.common import ParallelPolicy
+from py_trees import (
+    display as display_tree,
+    logging as log_tree
+)
+from etrobo_camera import Video, TraceSide
+
+INTERVAL: float = 0.04
+TIRE_DIAMETER: float = 100.0
+WHEEL_TREAD: float = 120.0
+
+g_distance: float = 0.0
+
+class Plotter(object):
+    def __init__(self) -> None:
+        self.running = False
+
+    def __call__(
+        self,
+        hub: Hub,
+        right_motor: Motor,
+        left_motor: Motor,
+        touch_sensor: TouchSensor,
+        color_sensor: ColorSensor,
+    ) -> None:
+        global g_distance
+        if not self.running:
+            self.running = True
+            right_motor.reset_count()
+            left_motor.reset_count()
+            self.prev_ang_r = right_motor.get_count()
+            self.prev_ang_l = left_motor.get_count()
+            g_distance = 0.0
+            return
+
+        cur_ang_r = right_motor.get_count()
+        cur_ang_l = left_motor.get_count()
+        delta_dist_r = math.pi * TIRE_DIAMETER * (cur_ang_r - self.prev_ang_r) / 360.0
+        delta_dist_l = math.pi * TIRE_DIAMETER * (cur_ang_l - self.prev_ang_l) / 360.0
+        delta_dist = (delta_dist_r + delta_dist_l / 2.0)
+        if (delta_dist >= 0.0):
+            g_distance += delta_dist
+        else:
+            g_distance -= delta_dist
+        self.prev_ang_r = cur_ang_r
+        self.prev_ang_l = cur_ang_l
+        return
+
+
+g_plotter: Plotter = None
+g_hub: Hub = None
+g_right_motor: Motor = None
+g_left_motor: Motor = None
+g_touch_sensor: TouchSensor = None
+g_color_sensor: ColorSensor = None
+g_video: Video = None
+g_video_thread: threading.Thread = None
+g_course: int = 0
+
+class TheEnd(Behaviour):
+    def __init__(self, name: str):
+        super(TheEnd, self).__init__(name)
+        self.logger.debug("%s.__init__()" % (self.__class__.__name__))
+        self.running = False
+
+    def update(self) -> Status:
+        if not self.running:
+            self.running = True
+            self.logger.info("%+06d %s.behavior tree exhausted. ctrl+C shall terminate the program" % (int(g_distance), self.__class__.__name__))
+        return Status.RUNNING
+
+
+class IsDistanceEarned(Behaviour):
+    def __init__(self, name: str, delta_dist: int):
+        super(IsDistanceEarned, self).__init__(name)
+        self.logger.debug("%s.__init__()" % (self.__class__.__name__))
+        self.delta_dist = delta_dist
+        self.running = False
+        self.earned = False
+
+    def update(self) -> Status:
+        global g_distance
+        if not self.running:
+            self.running = True
+            self.orig_dist = g_distance
+            self.logger.info("%+06d %s.distance accumulation started for delta=%d" % (int(g_distance), self.__class__.__name__, self.delta_dist))
+        earned_dist = g_distance - self.orig_dist
+        if (earned_dist >= self.delta_dist or -earned_dist <= -self.delta_dist):
+            if not self.earned:
+                self.earned = True
+                self.logger.info("%+06d %s.delta distance earned" % (int(g_distance), self.__class__.__name__))
+            return Status.SUCCESS
+        else:
+            return Status.RUNNING
+
+
+class IsTouchOn(Behaviour):
+    def __init__(self, name: str):
+        super(IsTouchOn, self).__init__(name)
+        self.logger.debug("%s.__init__()" % (self.__class__.__name__))
+
+    def update(self) -> Status:
+        global g_hub, g_touch_sensor
+        if (g_touch_sensor.is_pressed() or
+            g_hub.is_left_button_pressed() or
+            g_hub.is_right_button_pressed()):
+            self.logger.info("%+06d %s.touch sensor pressed" % (int(g_distance), self.__class__.__name__))
+            return Status.SUCCESS
+        else:
+            return Status.FAILURE
+
+
+class StopNow(Behaviour):
+    def __init__(self, name: str):
+        super(StopNow, self).__init__(name)
+        self.logger.debug("%s.__init__()" % (self.__class__.__name__))
+
+    def update(self) -> Status:
+        global g_right_motor, g_left_motor
+        g_right_motor.set_power(0)
+        g_left_motor.set_power(0)
+        self.logger.info("%+06d %s.motors stopped" % (int(g_distance), self.__class__.__name__))
+        return Status.SUCCESS
+
+
+class TraceLine(Behaviour):
+    def __init__(self, name: str, interval: float, target: int, power: int, pid_p: float, pid_i: float, pid_d: float,
+                 trace_side: TraceSide) -> None:
+        super(TraceLine, self).__init__(name)
+        self.power = power
+        self.pid = PID(pid_p, pid_i, pid_d, setpoint=target, sample_time=interval, output_limits=(-power, power))
+        self.trace_side = trace_side
+        self.running = False
+
+    def update(self) -> Status:
+        global g_color_sensor, g_right_motor, g_left_motor, g_course
+        if not self.running:
+            self.running = True
+            self.logger.info("%+06d %s.trace started with TS=%s" % (int(g_distance), self.__class__.__name__, self.trace_side.name))
+        if self.trace_side == TraceSide.NORMAL:
+            turn = (-1) * g_course * int(self.pid(g_color_sensor.get_brightness()))
+        else: # TraceSide.OPPOSITE
+            turn = g_course * int(self.pid(g_color_sensor.get_brightness()))
+        g_right_motor.set_power(self.power - turn)
+        g_left_motor.set_power(self.power + turn)
+        return Status.RUNNING
+
+
+class TraceLineCam(Behaviour):
+    def __init__(self, name: str, interval: float, power: int, pid_p: float, pid_i: float, pid_d: float,
+                 gs_min: int, gs_max: int, trace_side: TraceSide) -> None:
+        super(TraceLineCam, self).__init__(name)
+        self.power = power
+        self.pid = PID(pid_p, pid_i, pid_d, setpoint=0, sample_time=interval, output_limits=(-power, power))
+        self.gs_min = gs_min
+        self.gs_max = gs_max
+        self.trace_side = trace_side
+        self.running = False
+
+    def update(self) -> Status:
+        global g_video, g_right_motor, g_left_motor, g_course
+        if not self.running:
+            self.running = True
+            g_video.set_thresholds(self.gs_min, self.gs_max)
+            if self.trace_side == TraceSide.NORMAL:
+                if g_course == -1: # right course
+                    g_video.set_trace_side(TraceSide.RIGHT)
+                else:
+                    g_video.set_trace_side(TraceSide.LEFT)
+            elif self.trace_side == TraceSide.OPPOSITE: 
+                if g_course == -1: # right course
+                    g_video.set_trace_side(TraceSide.LEFT)
+                else:
+                    g_video.set_trace_side(TraceSide.RIGHT)
+            else: # TraceSide.CENTER
+                g_video.set_trace_side(TraceSide.CENTER)
+            self.logger.info("%+06d %s.trace started with TS=%s" % (int(g_distance), self.__class__.__name__, self.trace_side.name))
+        turn = (-1) * int(self.pid(g_video.get_theta()))
+        g_right_motor.set_power(self.power - turn)
+        g_left_motor.set_power(self.power + turn)
+        return Status.RUNNING
+
+
+class TraverseBehaviourTree(object):
+    def __init__(self, tree: BehaviourTree) -> None:
+        self.tree = tree
+    def __call__(
+        self,
+            **kwargs,
+    ) -> None:
+        self.tree.tick_once()
+    
+
+class ExposeDevices(object):
+    def __call__(
+        self,
+        hub: Hub,
+        right_motor: Motor,
+        left_motor: Motor,
+        touch_sensor: TouchSensor,
+        color_sensor: ColorSensor,
+    ) -> None:
+        global g_hub, g_right_motor, g_left_motor, g_touch_sensor, g_color_sensor
+        g_hub = hub
+        g_right_motor = right_motor
+        g_left_motor = left_motor
+        g_touch_sensor = touch_sensor
+        g_color_sensor = color_sensor
+
+
+class VideoThread(threading.Thread):
+    def __init__(self):
+        super().__init__()
+        self._stop_event = threading.Event()
+
+    def stop(self):
+        self._stop_event.set()
+
+    def run(self):
+        while not self._stop_event.is_set():
+            g_video.process()
+            time.sleep(0.02)
+
+
+def build_behaviour_tree() -> BehaviourTree:
+    root = Sequence(name="double loop neo", memory=True)
+#    section1 = Parallel(name="section 1", policy=ParallelPolicy.SuccessOnOne())
+    section2 = Parallel(name="section 2", policy=ParallelPolicy.SuccessOnOne())
+#    section1.add_children(
+#        [
+#            TraceLine(name="run", interval=INTERVAL, target=60, power=35, pid_p=0.56, pid_i=0.005, pid_d=0.015,
+#                      trace_side=TraceSide.NORMAL),
+#            IsDistanceEarned(name="check distance", delta_dist = 2000),
+#        ]
+#    )
+    section2.add_children(
+        [
+            TraceLineCam(name="run", interval=INTERVAL, power=33, pid_p=2.5, pid_i=0.0015, pid_d=0.1,
+                         gs_min=0, gs_max=80, trace_side=TraceSide.NORMAL),
+            IsDistanceEarned(name="check distance", delta_dist = 2000),
+        ]
+    )
+    root.add_children(
+        [
+            IsTouchOn(name="start"),
+#            section1,
+            section2,
+            StopNow(name="stop"),
+            TheEnd(name="end"),
+        ]
+    )
+    return root
+
+def initialize_etrobo(backend: str) -> ETRobo:
+    return (ETRobo(backend=backend)
+     .add_hub('hub')
+     .add_device('right_motor', device_type=Motor, port='B')
+     .add_device('left_motor', device_type=Motor, port='C')
+     .add_device('touch_sensor', device_type=TouchSensor, port='1')
+     .add_device('color_sensor', device_type=ColorSensor, port='2'))
+
+def setup():
+    global g_video, g_video_thread
+    g_video = Video()
+
+    print(" -- starting VideoThread...")
+    g_video_thread = VideoThread()
+    g_video_thread.start()
+
+def cleanup():
+    print(" -- stopping VideoThread...")
+    g_video_thread.stop()
+    g_video_thread.join()
+
+def sig_handler(signum, frame) -> None:
+    sys.exit(1)
+    
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('course', choices=['right', 'left'], help='Course to run')
+    parser.add_argument('--port', default='/dev/ttyAMA1', help='Serial port')
+    parser.add_argument('--logfile', type=str, default=None, help='Path to log file')
+    args = parser.parse_args()
+
+    if args.course == 'right':
+        g_course = -1
+    else:
+        g_course = 1
+
+    setup()
+
+    #py_trees.logging.level = py_trees.logging.Level.DEBUG
+    tree = build_behaviour_tree()
+    display_tree.render_dot_tree(tree)
+
+    signal.signal(signal.SIGTERM, sig_handler)
+
+    try:
+        etrobo = initialize_etrobo(backend='raspike')
+        etrobo.add_handler(ExposeDevices())
+        etrobo.add_handler(Plotter())
+        etrobo.add_handler(TraverseBehaviourTree(tree))
+        etrobo.dispatch(interval=INTERVAL, port=args.port, logfile=args.logfile)
+    finally:
+        signal.signal(signal.SIGTERM, signal.SIG_IGN)
+        signal.signal(signal.SIGINT, signal.SIG_IGN)
+        cleanup()
+        signal.signal(signal.SIGTERM, signal.SIG_DFL)
+        signal.signal(signal.SIGINT, signal.SIG_DFL)
+        print(" -- exiting...")
---
+++ 2024base/etrobo_camera/video.py
@@ -0,0 +1,207 @@
+import cv2
+import math
+import numpy as np
+from enum import Enum
+
+def round_up_to_odd(f) -> int:
+    return int(np.ceil(f / 2.) * 2 + 1)
+
+# frame size for Raspberry Pi camera capture
+IN_FRAME_WIDTH  = 640
+IN_FRAME_HEIGHT = 480
+
+# frame size for OpenCV
+#FRAME_WIDTH  = 160
+#FRAME_HEIGHT = 120
+FRAME_WIDTH  = 320
+FRAME_HEIGHT = 240
+
+#CROP_WIDTH     = int(13*FRAME_WIDTH/16) # for full angle
+CROP_WIDTH     = int(9*FRAME_WIDTH/16)
+#CROP_HEIGHT    = int(3*FRAME_HEIGHT/8) # for full angle
+CROP_HEIGHT    = int(2*FRAME_HEIGHT/8)
+CROP_U_LIMIT   = FRAME_HEIGHT-CROP_HEIGHT
+CROP_D_LIMIT   = FRAME_HEIGHT
+CROP_L_LIMIT   = int((FRAME_WIDTH-CROP_WIDTH)/2)
+CROP_R_LIMIT   = (CROP_L_LIMIT+CROP_WIDTH)
+MORPH_KERNEL_SIZE = round_up_to_odd(int(FRAME_WIDTH/48))
+ROI_BOUNDARY   = int(FRAME_WIDTH/10)
+LINE_THICKNESS = int(FRAME_WIDTH/80)
+CIRCLE_RADIUS  = int(FRAME_WIDTH/40)
+#SCAN_V_POS     = int(13*FRAME_HEIGHT/16 - LINE_THICKNESS) # for full angle
+SCAN_V_POS     = int(16*FRAME_HEIGHT/16 - LINE_THICKNESS)
+
+# frame size for X11 painting
+OUT_FRAME_WIDTH  = 160
+OUT_FRAME_HEIGHT = 120
+
+
+class TraceSide(Enum):
+    NORMAL = "Normal"
+    OPPOSITE = "Opposite"
+    RIGHT = "Right"
+    LEFT = "Left"
+    CENTER = "Center"
+
+
+class Video(object):
+    def __init__(self):
+        cv2.setLogLevel(3) # LOG_LEVEL_WARNING
+        # set number of threads
+        #cv2.setNumThreads(0)
+        # prepare the camera
+        self.cap = cv2.VideoCapture(0)
+        self.cap.set(cv2.CAP_PROP_FRAME_WIDTH,IN_FRAME_WIDTH)
+        self.cap.set(cv2.CAP_PROP_FRAME_HEIGHT,IN_FRAME_HEIGHT)
+        self.cap.set(cv2.CAP_PROP_FPS,90)
+
+        # initial region of interest
+        self.roi = (CROP_L_LIMIT, CROP_U_LIMIT, CROP_WIDTH, CROP_HEIGHT)
+        # prepare and keep kernel for morphology
+        self.kernel = np.ones((MORPH_KERNEL_SIZE,MORPH_KERNEL_SIZE), np.uint8)
+        # initial trace target
+        self.cx = int(FRAME_WIDTH/2)
+        self.cy = SCAN_V_POS
+        self.mx = self.cx
+        # default values
+        self.gsmin = 0
+        self.gsmax = 100
+        self.trace_side = TraceSide.NORMAL
+        self.theta:float = 0
+        self.target_insight = False
+
+    def __del__(self):
+        cv2.destroyAllWindows
+        self.cap.release()
+
+
+    def process(self) -> None:
+        ret, frame = self.cap.read()
+
+        # clone the image if exists, otherwise use the previous image
+        if len(frame) != 0:
+            img_orig = frame.copy()
+            # resize the image for OpenCV processing
+        if FRAME_WIDTH != IN_FRAME_WIDTH or FRAME_HEIGHT != IN_FRAME_HEIGHT:
+            img_orig = cv2.resize(img_orig, (FRAME_WIDTH,FRAME_HEIGHT))
+            if img_orig.shape[1] != FRAME_WIDTH or img_orig.shape[0] != FRAME_HEIGHT:
+                sys.exit(-1)
+        # convert the image from BGR to grayscale
+        img_gray = cv2.cvtColor(img_orig, cv2.COLOR_BGR2GRAY)
+        # crop a part of image for binarization
+        img_gray_part = img_gray[CROP_U_LIMIT:CROP_D_LIMIT, CROP_L_LIMIT:CROP_R_LIMIT]
+        # binarize the image
+        img_bin_part = cv2.inRange(img_gray_part, self.gsmin, self.gsmax)
+        # prepare an empty matrix
+        img_bin = np.zeros((FRAME_HEIGHT, FRAME_WIDTH), np.uint8)
+        # copy img_bin_part into img_bin
+        img_bin[CROP_U_LIMIT:CROP_U_LIMIT+img_bin_part.shape[0], CROP_L_LIMIT:CROP_L_LIMIT+img_bin_part.shape[1]] = img_bin_part
+        # remove noise
+        img_bin_mor = cv2.morphologyEx(img_bin, cv2.MORPH_CLOSE, self.kernel)
+        # convert the binary image from grayscale to BGR for later
+        img_bin_rgb = cv2.cvtColor(img_bin_mor, cv2.COLOR_GRAY2BGR)
+
+        # focus on the region of interest
+        x, y, w, h = self.roi
+        img_roi = img_bin_mor[y:y+h, x:x+w]
+        # find contours in the roi with offset
+        contours, hierarchy = cv2.findContours(img_roi, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE, offset=(x,y))
+        # identify the largest contour
+        if len(contours) >= 1:
+            i_area_max = 0
+            area_max = 0
+            for i, cnt in enumerate(contours):
+                area = cv2.contourArea(cnt)
+                if area > area_max:
+                    area_max = area
+                    i_area_max = i
+            # draw the largest contour on the original image
+            #img_orig = cv2.drawContours(img_orig, [contours[i_area_max]], 0, (0,255,0), LINE_THICKNESS)
+            img_orig = cv2.polylines(img_orig, [contours[i_area_max]], 0, (0,255,0), LINE_THICKNESS)
+
+            # calculate the bounding box around the largest contour
+            # and set it as the new region of interest
+            x, y, w, h = cv2.boundingRect(contours[i_area_max])
+            # adjust the region of interest
+            x = x - ROI_BOUNDARY
+            y = y - ROI_BOUNDARY
+            w = w + 2*ROI_BOUNDARY
+            h = h + 2*ROI_BOUNDARY
+            if x < CROP_L_LIMIT:
+                x = CROP_L_LIMIT
+            if y < CROP_U_LIMIT:
+                y = CROP_U_LIMIT
+            if x + w > CROP_R_LIMIT:
+                w = CROP_R_LIMIT - x
+            if y + h > CROP_D_LIMIT:
+                h = CROP_D_LIMIT - y
+            # set the new region of interest
+            self.roi = (x, y, w, h)
+        
+            # prepare for trace target calculation
+            img_cnt = np.zeros_like(img_orig)
+            img_cnt = cv2.drawContours(img_cnt, [contours[i_area_max]], 0, (0,255,0), 1)
+            img_cnt_gray = cv2.cvtColor(img_cnt, cv2.COLOR_BGR2GRAY)
+            # scan the line at SCAN_V_POS to find edges
+            scan_line = img_cnt_gray[SCAN_V_POS]
+            edges = np.flatnonzero(scan_line)
+            # calculate the trace target using the edges
+            if len(edges) >= 2:
+                if self.trace_side == TraceSide.LEFT:
+                    self.cx = edges[0]
+                elif self.trace_side == TraceSide.RIGHT:
+                    self.cx = edges[len(edges)-1]
+                else:
+                    self.cx = int((edges[0]+edges[len(edges)-1]) / 2)
+            elif len(edges) == 1:
+                self.cx = edges[0]
+            self.mx = self.cx
+            self.target_insight = True
+        else: # len(contours) == 0
+            self.roi = (CROP_L_LIMIT, CROP_U_LIMIT, CROP_WIDTH, CROP_HEIGHT)
+            # keep mx in order to maintain the current move of robot
+            self.cx = int(FRAME_WIDTH/2)
+            self.cy = SCAN_V_POS
+            self.target_insight = False
+            
+
+        # draw the area of interest on the original image
+        x, y, w, h = self.roi
+        if self.target_insight:
+            cv2.rectangle(img_orig, (x,y), (x+w,y+h), (255,0,0), LINE_THICKNESS)
+        else:
+            cv2.rectangle(img_orig, (x,y), (x+w,y+h), (0,0,255), LINE_THICKNESS)
+        # draw the trace target on the image
+        cv2.circle(img_orig, (self.mx, SCAN_V_POS), CIRCLE_RADIUS, (0,0,255), -1)
+        # calculate variance of mx from the center in pixel
+        vxp = self.mx - int(FRAME_WIDTH/2)
+        # convert the variance from pixel to milimeters
+        # 138 is length of the closest horizontal line on ground within the camera vision
+        vxm = vxp * 138 / FRAME_WIDTH
+        # calculate the rotation in radians (z-axis)
+        # 205 is distance from axle to the closest horizontal line on ground the camera can see
+        self.theta = 180 * math.atan(vxm / 205) / math.pi
+        #print(f"mx = {self.mx}, vxm = {vxm}, theta = {self.theta}")
+
+        # prepare text area
+        img_text = np.zeros((FRAME_HEIGHT, FRAME_WIDTH, 3), np.uint8)
+        cv2.putText(img_text, f"cx={self.cx:} cy={self.cy} T={self.theta:+06.1f}", (0,10), cv2.FONT_HERSHEY_PLAIN, 1, (255,255,255), 1, cv2.LINE_AA)
+        # concatinate the images - original + text area
+        img_comm = cv2.vconcat([img_orig,img_text])
+        # shrink the image to avoid delay in transmission
+        if OUT_FRAME_WIDTH != FRAME_WIDTH or OUT_FRAME_HEIGHT != FRAME_HEIGHT:
+            img_comm = cv2.resize(img_comm, (OUT_FRAME_WIDTH,2*OUT_FRAME_HEIGHT))
+        # transmit and display the image
+        cv2.imshow("video monitor", img_comm)
+
+        c = cv2.waitKey(1) # show the window
+        
+    def get_theta(self) -> float:
+        return self.theta
+
+    def set_thresholds(self, gs_min: int, gs_max: int) -> None:
+        self.gs_min = gs_min
+        self.gs_max = gs_max
+
+    def set_trace_side(self, trace_side: TraceSide) -> None:
+        self.trace_side = trace_side
