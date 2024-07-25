18999280c8e572e699e4f1f6d26fab1917abbc0c
shogo1224 <shogo4933@gmail.com>
2024-07-25 20:31:50 +0900

    2025base2

--- msad2024_teamB/behaviorTreeCameraPID_base.py
+++ msad2024_teamB/behaviorTreeCameraPID_base.py
@@ -392,7 +392,7 @@ def build_behaviour_tree() -> BehaviourTree:
         [
             TraceLineCam(name="trace normal edge", power=40, pid_p=2.5, pid_i=0.0015, pid_d=0.1,
                          gs_min=0, gs_max=80, trace_side=TraceSide.NORMAL),
-            IsDistanceEarned(name="check distance", delta_dist = 2000),
+            IsDistanceEarned(name="check distance", delta_dist = 10),
         ]
     )
     loop_02.add_children(
