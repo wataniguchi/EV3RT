cceee595f449c4cc09b5a0f62dfadc9b56f9f658
shogo1224 <shogo4933@gmail.com>
2024-07-25 19:42:34 +0900

    0725_15

--- msad2024_teamB/behaviorTreeCameraPID2.py
+++ msad2024_teamB/behaviorTreeCameraPID2.py
@@ -318,7 +318,7 @@ class IsColorDetected(Behaviour):
         color = g_color_sensor.get_raw_color()
         print(color)
         #Blue判定
-        if(color[2] - color[0]>45 & color[2] <=255 & color[0] <=255):
+        if(color[2] - color[0]>80 & color[2] <=255 & color[0] <=255):
             self.logger.info("%+06d %s.detected blue" % (g_plotter.get_distance(), self.__class__.__name__))
             return Status.SUCCESS
         else:
