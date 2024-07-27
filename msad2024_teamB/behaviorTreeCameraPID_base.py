42f4dead73c62a4d23282281af0be6656efa6ec4
KOTORINEKO <research.for.the.a@gmail.com>
2024-07-27 14:12:41 +0900

    0727_21

--- msad2024_teamB/behaviorTreeCameraPID_base.py
+++ msad2024_teamB/behaviorTreeCameraPID_base.py
@@ -420,7 +420,7 @@ def build_behaviour_tree() -> BehaviourTree:
     )
     loop_05.add_children(
         [
-            TraceLineCam(name="trace normal edge", power=45, pid_p=2.0, pid_i=0.0011, pid_d=0.35,
+            TraceLineCam(name="trace normal edge", power=40, pid_p=2.0, pid_i=0.0011, pid_d=0.35,
                          gs_min=0, gs_max=80, trace_side=TraceSide.NORMAL),
             IsJunction(name="scan joined junction", target_state = JState.JOINED),
         ]
