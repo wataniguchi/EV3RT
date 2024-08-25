94fc30c990a684977fa810a91e875eab794df8ef
KOTORINEKO <research.for.the.a@gmail.com>
2024-08-25 10:41:22 +0900

    0825_test4

--- msad2024_teamB/behaviorTreeCameraPID_base.py
+++ msad2024_teamB/behaviorTreeCameraPID_base.py
@@ -395,7 +395,7 @@ def build_behaviour_tree() -> BehaviourTree:
     )
     loop_01.add_children(
         [
-            TraceLineCam(name="trace normal edge", power=70, pid_p=1.25, pid_i=0.0015, pid_d=0.4,
+            TraceLineCam(name="trace normal edge", power=50, pid_p=1.5, pid_i=0.0015, pid_d=0.4,
                          gs_min=0, gs_max=80, trace_side=TraceSide.NORMAL),
             IsDistanceEarned(name="check distance", delta_dist = 2500),
         ]
@@ -403,7 +403,7 @@ def build_behaviour_tree() -> BehaviourTree:
 
     loop_01_1.add_children(
         [
-            TraceLineCam(name="trace normal edge", power=60, pid_p=1.25, pid_i=0.0015, pid_d=0.4,
+            TraceLineCam(name="trace normal edge", power=50, pid_p=1.5, pid_i=0.0015, pid_d=0.4,
                          gs_min=0, gs_max=80, trace_side=TraceSide.NORMAL),
             IsDistanceEarned(name="check distance", delta_dist = 1000),
         ]
