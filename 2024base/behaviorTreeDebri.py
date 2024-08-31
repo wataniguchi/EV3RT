68d08741032f863abb09ff4be24fcca923b85548
lxmd4 <175673095+lxmd4@users.noreply.github.com>
2024-08-31 15:24:58 +0900

    debri

--- 2024base/behaviorTreeDebri.py
+++ 2024base/behaviorTreeDebri.py
@@ -411,7 +411,7 @@ def build_behaviour_tree() -> BehaviourTree:
 
     cross_task01.add_children([
         RunAsInstructed(name="cross circle", pwm_r=38, pwm_l=38),
-        IsDistanceEarned(name="check distance", delta_dist=450),
+        IsDistanceEarned(name="check distance", delta_dist=250),
     ])
 
     end_debri.add_children([
