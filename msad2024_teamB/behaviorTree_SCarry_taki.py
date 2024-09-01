dfe10fb644f2dfe7de16a31459b08918dd78b5d1
KOTORINEKO <research.for.the.a@gmail.com>
2024-09-01 16:34:09 +0900

    SmartCarry taki-test 20240901

--- msad2024_teamB/behaviorTree_SCarry_taki.py
+++ msad2024_teamB/behaviorTree_SCarry_taki.py
@@ -546,7 +546,7 @@ def build_behaviour_tree() -> BehaviourTree:
         [
             TraceLineCam(name="trace buleline", power=50, pid_p=2.5, pid_i=0.0015, pid_d=0.1,
                  gs_min=0, gs_max=80, trace_side=TraceSide.NORMAL),
-            IsDistanceEarned(name="check distance 1", delta_dist = 1100)
+            IsDistanceEarned(name="check distance 1", delta_dist = 400)
         ]
     )
     # step_01B.add_children(
