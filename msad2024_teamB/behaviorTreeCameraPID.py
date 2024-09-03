a5e94d146e3687435ea43e703a13e05955045af8
yukitake-9908 <yukibaseball.9908@gmail.com>
2024-09-03 19:07:29 +0900

    0903_04

--- msad2024_teamB/behaviorTreeCameraPID.py
+++ msad2024_teamB/behaviorTreeCameraPID.py
@@ -533,17 +533,17 @@ def build_behaviour_tree() -> BehaviourTree:
     )
     loop_08.add_children(
         [
-            TraceLineCam(name="trace normal edge", power=60, pid_p=2.5, pid_i=0.0015, pid_d=0.1,
+            TraceLineCam(name="trace normal edge", power=40, pid_p=1.0, pid_i=0.0015, pid_d=0.1,
                          gs_min=0, gs_max=80, trace_side=TraceSide.NORMAL),
-            IsDistanceEarned(name="check distance", delta_dist = 10000),
-        ]
-    )
-    loop_09.add_children(
-        [
-             MoveStraight(name="move straight 3", power=55, target_distance=2000),
             IsDistanceEarned(name="check distance", delta_dist = 1300),
         ]
     )
+    # loop_09.add_children(
+    #     [
+    #          MoveStraight(name="move straight 3", power=55, target_distance=2000),
+    #         IsDistanceEarned(name="check distance", delta_dist = 1300),
+    #     ]
+    # )
     # loop_09.add_children(
     #     [
     #         TraceLineCam(name="trace normal edge", power=-40, pid_p=2.5, pid_i=0.0015, pid_d=0.1,
@@ -551,12 +551,12 @@ def build_behaviour_tree() -> BehaviourTree:
     #         IsDistanceEarned(name="check distance", delta_dist = 150),
     #     ]
     # )
-    loop_10.add_children(
-        [
-            MoveStraightLR(name="move straight 4", right_power=0, left_power=80, target_distance=444),
-            IsDistanceEarned(name="check distance", delta_dist = 190),
-        ]
-    )
+    # loop_10.add_children(
+    #     [
+    #         MoveStraightLR(name="move straight 4", right_power=0, left_power=80, target_distance=444),
+    #         IsDistanceEarned(name="check distance", delta_dist = 190),
+    #     ]
+    # )
     # loop_11.add_children(
     #     [
     #         TraceLineCam(name="trace normal edge", power=40, pid_p=2.5, pid_i=0.0015, pid_d=0.1,
@@ -577,8 +577,8 @@ def build_behaviour_tree() -> BehaviourTree:
             # loop_07,
             #W-loop_end
             loop_08,
-            loop_09,
-            loop_10,
+            # loop_09,
+            # loop_10,
             # loop_11,
             StopNow(name="stop"),
             TheEnd(name="end"),
