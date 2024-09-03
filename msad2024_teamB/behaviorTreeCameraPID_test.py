96c659cb3caf994a35d8bdfce1d1a55173dcede8
ShiryuK <shiryu2000note@yahoo.co.jp>
2024-09-03 19:00:49 +0900

    0903_1

--- msad2024_teamB/behaviorTreeCameraPID_test.py
+++ msad2024_teamB/behaviorTreeCameraPID_test.py
@@ -406,7 +406,7 @@ def build_behaviour_tree() -> BehaviourTree:
     #最初のストレート減速1段階目
     loop_01_1.add_children(
         [
-            TraceLineCam(name="trace normal edge", power=60, pid_p=1.5, pid_i=0.0015, pid_d=0.4,
+            TraceLineCam(name="trace normal edge", power=60, pid_p=1.2, pid_i=0.0015, pid_d=0.4,
                          gs_min=0, gs_max=80, trace_side=TraceSide.NORMAL),
             IsDistanceEarned(name="check distance", delta_dist = 1000),
         ]
