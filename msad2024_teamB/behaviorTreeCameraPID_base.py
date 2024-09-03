983dba5bf8a1093bc3fa84756ea2b5d1336c5e37
KOTORINEKO <research.for.the.a@gmail.com>
2024-09-03 21:01:07 +0900

    0903_4

--- msad2024_teamB/behaviorTreeCameraPID_base.py
+++ msad2024_teamB/behaviorTreeCameraPID_base.py
@@ -406,7 +406,7 @@ def build_behaviour_tree() -> BehaviourTree:
     #最初のストレート最高速度
     loop_01.add_children(
         [
-            TraceLineCam(name="trace normal edge", power=70, pid_p=1.5, pid_i=0.0015, pid_d=0.4,
+            TraceLineCam(name="trace normal edge", power=60, pid_p=1.5, pid_i=0.0015, pid_d=0.4,
                          gs_min=0, gs_max=80, trace_side=TraceSide.NORMAL),
             IsDistanceEarned(name="check distance", delta_dist = 1000),
         ]
