7c7ff25558dfe479be8c9aa20014922bf5bb67e7
ShiryuK <shiryu2000note@yahoo.co.jp>
2024-09-05 21:23:12 +0900

    0905_13

--- msad2024_teamB/behaviorTreeCameraPID_test.py
+++ msad2024_teamB/behaviorTreeCameraPID_test.py
@@ -446,13 +446,13 @@ def build_behaviour_tree() -> BehaviourTree:
         [
             TraceLineCam(name="trace normal edge", power=50, pid_p=1.5, pid_i=0.001, pid_d=0.30,
                          gs_min=0, gs_max=80, trace_side=TraceSide.NORMAL),
-            IsDistanceEarned(name="check distance", delta_dist = 800),
+            IsDistanceEarned(name="check distance", delta_dist = 1000),
         ]
     )
     #コンタクトⅠ直前
     loop_02_1.add_children(
         [
-            TraceLineCam(name="trace normal edge", power=35, pid_p=1.5, pid_i=0.001, pid_d=0.30,
+            TraceLineCam(name="trace normal edge", power=60, pid_p=1.5, pid_i=0.001, pid_d=0.30,
                          gs_min=0, gs_max=80, trace_side=TraceSide.NORMAL),
             IsJunction(name="scan joined junction", target_state = JState.JOINED),
         ]
