8a09116b3331ddd559655e1f7f853a2ef547998c
KOTORINEKO <research.for.the.a@gmail.com>
2024-09-05 20:25:58 +0900

    Scarry 20240905 taki

--- msad2024_teamB/behaviorTree_SCarry_AN2_taki.py
+++ msad2024_teamB/behaviorTree_SCarry_AN2_taki.py
@@ -316,17 +316,21 @@ class TraceLineCam(Behaviour):
 
 # 旧Wループプログラムから流用
 class IsColorDetected(Behaviour):
-    def __init__(self, name: str):
+    def __init__(self, name: str, target_color: Color) :
         super(IsColorDetected, self).__init__(name)
+        self.status:Color = Color.BLACK
         self.logger.debug("%s.__init__()" % (self.__class__.__name__))
 
     def update(self) -> Status:
-        global g_color_sensor
         #RGBの値を取得
         color = g_color_sensor.get_raw_color
+        self.logger.info("%+06d %s.detected blue" % (color, self.__class__.__name__))
+        if color == (color[2] - color[0]>45 & color[2] <=255 & color[0] <=255):
+            self.status = Color.BLUE
         #Blue判定
-        if(color[2] - color[0]>45 & color[2] <=255 & color[0] <=255):
-            self.logger.info("%+06d %s.detected blue" % (g_plotter.get_distance(), self.__class__.__name__))
+        if self.status == Color.BLUE:
+        # if(color[2] - color[0]>45 & color[2] <=255 & color[0] <=255):
+            self.logger.info("%+06d %s.detected blue" % (g_color_sensor.get_raw_color(), self.__class__.__name__))
             return Status.SUCCESS
         else:
             #指定色でないならRUNNINGを返却
@@ -496,11 +500,12 @@ def build_behaviour_tree() -> BehaviourTree:
     root = Sequence(name="competition", memory=True)
     calibration = Sequence(name="calibration", memory=True)
     start = Parallel(name="start", policy=ParallelPolicy.SuccessOnOne())
-    step_01B = Parallel(name="step 01B", policy=ParallelPolicy.SuccessOnOne())
-    step_01B_2 = Parallel(name="step 01B", policy=ParallelPolicy.SuccessOnOne())
-    step_02B = Sequence(name="step 02B", memory=True)
-    step_03B = Sequence(name="step 03B", memory=True)
-    step_04B = Sequence(name="step 04B", memory=True)
+    # step_01B = Parallel(name="step 01B", policy=ParallelPolicy.SuccessOnOne())
+    # step_01B_2 = Parallel(name="step 01B", policy=ParallelPolicy.SuccessOnOne())
+    # step_02B = Sequence(name="step 02B", memory=True)
+    # step_03B = Sequence(name="step 03B", memory=True)
+    # step_04B = Sequence(name="step 04B", memory=True)
+    step_04B = Sequence(name="step 04B", policy=ParallelPolicy.SuccessOnOne())
  
     calibration.add_children(
         [
@@ -514,46 +519,47 @@ def build_behaviour_tree() -> BehaviourTree:
             IsTouchOn(name="touch start"),
         ]
     )
-    # デブリからボトル取得
-    step_01B.add_children(
-        [
-            TraceLineCam(name="trace buleline", power=40, pid_p=2.5, pid_i=0.0015, pid_d=0.1,
-                 gs_min=0, gs_max=80, trace_side=TraceSide.NORMAL),
-            Bottlecatch(name="linetrace", target_state = BState.LINE)
-            #IsDistanceEarned(name="check distance 1", delta_dist = 400)
-        ]
-    )
-
+    # # デブリからボトル取得
     # step_01B.add_children(
     #     [
-    #         MoveStraight(name="free run 1", power=50, target_distance=450)
-    #         # IsSonarOn(name="check bottol", alert_dist=150)
+    #         TraceLineCam(name="trace buleline", power=40, pid_p=2.5, pid_i=0.0015, pid_d=0.1,
+    #              gs_min=0, gs_max=80, trace_side=TraceSide.NORMAL),
+    #         Bottlecatch(name="linetrace", target_state = BState.LINE)
+    #         #IsDistanceEarned(name="check distance 1", delta_dist = 400)
     #     ]
     # )
-    # ボトル取得からサークルへ配置
-    step_02B.add_children(
-        [
-            MoveStraightLR(name="Turn 1", right_power=0, left_power=70, target_distance=200),
-            MoveStraight(name="free run 2", power=50, target_distance=1200)
-            # color sensor add
-        ]
-    )
-    # サークルへ配置からライン復帰
-    step_03B.add_children(
-        [
-            MoveStraight(name="back", power=-70, target_distance=200),
-            MoveStraightLR(name="Turn 2", right_power=70, left_power=0, target_distance=200),
-            MoveStraight(name="free run 3", power=50, target_distance=400)
-        ]
-    )
+
+    # # step_01B.add_children(
+    # #     [
+    # #         MoveStraight(name="free run 1", power=50, target_distance=450)
+    # #         # IsSonarOn(name="check bottol", alert_dist=150)
+    # #     ]
+    # # )
+    # # ボトル取得からサークルへ配置
+    # step_02B.add_children(
+    #     [
+    #         MoveStraightLR(name="Turn 1", right_power=0, left_power=70, target_distance=200),
+    #         MoveStraight(name="free run 2", power=50, target_distance=1200)
+    #         # color sensor add
+    #     ]
+    # )
+    # # サークルへ配置からライン復帰
+    # step_03B.add_children(
+    #     [
+    #         MoveStraight(name="back", power=-70, target_distance=200),
+    #         MoveStraightLR(name="Turn 2", right_power=70, left_power=0, target_distance=200),
+    #         MoveStraight(name="free run 3", power=50, target_distance=400)
+    #     ]
+    # )
 
     # ライン復帰からゴール
     step_04B.add_children(
         [
-            MoveStraightLR(name="Turn 3", right_power=70, left_power=35, target_distance=200),
+            # MoveStraightLR(name="Turn 3", right_power=70, left_power=35, target_distance=200),
             TraceLineCam(name="trace center edge", power=40, pid_p=2.5, pid_i=0.0015, pid_d=0.1,
                          gs_min=0, gs_max=80, trace_side=TraceSide.CENTER),
-            IsDistanceEarned(name="check distance 1", delta_dist = 1100)
+            IsDistanceEarned(name="check distance 1", delta_dist = 1100),
+            IsColorDetected(name="color",color=Color.BLUE)
             # color sensor add
         ]
     )
@@ -562,10 +568,10 @@ def build_behaviour_tree() -> BehaviourTree:
         [
             calibration,
             start,
-            step_01B,
-            step_01B_2,
-            step_02B,
-            step_03B,
+            # step_01B,
+            # step_01B_2,
+            # step_02B,
+            # step_03B,
             step_04B,
             StopNow(name="stop"),
             TheEnd(name="end"),
