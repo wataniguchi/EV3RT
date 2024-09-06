d000f6c44ef731a6e948ca0affec4edd11f4d9c9
Roy <saya22ih0734@gmail.com>
2024-09-06 14:24:38 +0900

    01A_4 power 32 -> 40

--- msad2024_teamB/behaviorTree_SCarry_AN2.py
+++ msad2024_teamB/behaviorTree_SCarry_AN2.py
@@ -562,7 +562,7 @@ def build_behaviour_tree() -> BehaviourTree:
     )
     step_01A_4.add_children(
         [
-            TraceLineCam(name="trace buleline4", power=32, pid_p=2.5, pid_i=0.0015, pid_d=0.1,
+            TraceLineCam(name="trace buleline4", power=40, pid_p=2.5, pid_i=0.0015, pid_d=0.1,
                  gs_min=0, gs_max=80, trace_side=TraceSide.CENTER),
             #IsDistanceEarned(name="check distance 1", delta_dist = 200),
             Bottlecatch(name="trace CATCHED", target_state = BState.CATCHED)
