49bdf835d86371c4134ab4522803dabfe675d396
ShiryuK <shiryu2000note@yahoo.co.jp>
2024-09-07 23:35:08 +0900

    wloop_ms_v3_20240907

--- msad2024_teamB/behaviorTreeCameraPID_test2_v3.py
+++ msad2024_teamB/behaviorTreeCameraPID_test2_v3.py
@@ -408,7 +408,7 @@ def build_behaviour_tree() -> BehaviourTree:
     #最初のストレート最高速度(助走2_ms)
     loop_01_01.add_children(
         [
-            TraceLineCam(name="trace normal edge", power=50, pid_p=1.5, pid_i=0.0015, pid_d=0.4,
+            TraceLineCam(name="trace normal edge", power=55, pid_p=1.5, pid_i=0.0015, pid_d=0.4,
                          gs_min=0, gs_max=80, trace_side=TraceSide.NORMAL),
             IsDistanceEarned(name="check distance", delta_dist = 200),
         ]
@@ -417,7 +417,9 @@ def build_behaviour_tree() -> BehaviourTree:
     #最初のストレート最高速度
     loop_01_02.add_children(
         [
-            TraceLineCam(name="trace normal edge", power=60, pid_p=1.5, pid_i=0.0015, pid_d=0.4,
+    #        TraceLineCam(name="trace normal edge", power=70, pid_p=1.5, pid_i=0.0015, pid_d=0.4,
+                         
+            TraceLineCam(name="trace normal edge", power=70, pid_p=2.0, pid_i=0.0015, pid_d=0.4,
                          gs_min=0, gs_max=80, trace_side=TraceSide.NORMAL),
             IsDistanceEarned(name="check distance", delta_dist = 2100),
         ]
@@ -499,7 +501,7 @@ def build_behaviour_tree() -> BehaviourTree:
         [
             TraceLineCam(name="trace normal edge", power=45, pid_p=2.0, pid_i=0.0015, pid_d=0.3,
                          gs_min=0, gs_max=80, trace_side=TraceSide.NORMAL),
-            IsDistanceEarned(name="check distance", delta_dist = 3000),
+            IsDistanceEarned(name="check distance", delta_dist = 1000),
         ]
     )
     root.add_children(
