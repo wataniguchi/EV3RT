84b1b7325f73e1bafe3f43c24f7ee7eb4d65fb78
yukitake-9908 <yukibaseball.9908@gmail.com>
2024-09-07 20:09:58 +0900

    0907_112

--- msad2024_teamB/behaviorTreeCameraPID.py
+++ msad2024_teamB/behaviorTreeCameraPID.py
@@ -496,8 +496,8 @@ class IsBlueColorDetected(Behaviour):
         if blue_percentage > self.threshold:
             self.logger.info("%+06d %s.blue color ratio exceeds threshold: %f" % (g_plotter.get_distance(), self.__class__.__name__, blue_percentage))
             g_dist = g_dist - g_earned_dist
-            print("g_earned_dist:"+ g_earned_dist)
-            print("g_dist:"+ g_dist)
+            print(g_earned_dist)
+            print(g_dist)
             return Status.SUCCESS
         else:
             return Status.RUNNING
