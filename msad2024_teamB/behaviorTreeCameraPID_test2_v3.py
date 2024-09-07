8ff674f063f09bf00cf8b3b8654e7fb0e8d31e51
ShiryuK <shiryu2000note@yahoo.co.jp>
2024-09-08 00:02:07 +0900

    wloop_ms_v3_3_20240907

--- msad2024_teamB/behaviorTreeCameraPID_test2_v3.py
+++ msad2024_teamB/behaviorTreeCameraPID_test2_v3.py
@@ -418,8 +418,10 @@ def build_behaviour_tree() -> BehaviourTree:
     loop_01_02.add_children(
         [
     #        TraceLineCam(name="trace normal edge", power=70, pid_p=1.5, pid_i=0.0015, pid_d=0.4,
+    #        pid_p=1.5→2.0→1,7
+
                          
-            TraceLineCam(name="trace normal edge", power=70, pid_p=2.0, pid_i=0.0015, pid_d=0.4,
+            TraceLineCam(name="trace normal edge", power=70, pid_p=1.7, pid_i=0.0015, pid_d=0.4,
                          gs_min=0, gs_max=80, trace_side=TraceSide.NORMAL),
             IsDistanceEarned(name="check distance", delta_dist = 2100),
         ]
