8c1414803ff4c840f121264f7a431be0bd40284c
yukitake-9908 <yukibaseball.9908@gmail.com>
2024-09-07 17:33:41 +0900

    0907_84

--- msad2024_teamB/behaviorTreeCameraPID.py
+++ msad2024_teamB/behaviorTreeCameraPID.py
@@ -508,9 +508,9 @@ class IsDistanceEarned_before(Behaviour):
         self.logger.debug("%s.__init__()" % (self.__class__.__name__))
         self.delta_dist = delta_dist
         self.running = False
-        global g_earned_dist
 
     def update(self) -> Status:
+        global g_earned_dist
         if not self.running:
             self.running = True
             self.orig_dist = g_plotter.get_distance()
