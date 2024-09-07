58cd7fb6623b8a4624c84040e8d1835e3cba75f4
mt-robo <kait8tajiri5m@gmail.com>
2024-09-07 16:09:37 +0900

    add

--- msad2024_teamB/behaviorTree_SCarry_AN2_taji.py
+++ msad2024_teamB/behaviorTree_SCarry_AN2_taji.py
@@ -329,7 +329,7 @@ class IsColorDetected(Behaviour):
         color = g_color_sensor.get_raw_color()
         #Blue判定
         if self.name == "blue" :
-            if((color[2] > 200) & (color[2] <=255) & (color[0] <=128) & (color[1] <=128)):
+            if((color[2] > 170) & (color[0] <=128) & (color[1] <=128)):
             #if((color[2] - color[0]>45) & (color[2] <=255) & (color[0] <=255)):
                 self.logger.info("%+06d %s.detected blue" % (g_plotter.get_distance(), self.__class__.__name__))
                 self.logger.info("%+06d %s.blue r=%d g=%d b=%d" % (g_plotter.get_distance(), self.__class__.__name__, color[0], color[1], color[2]))
