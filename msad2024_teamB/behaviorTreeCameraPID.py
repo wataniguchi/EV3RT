ca5cfaaf0d6ef406f7130ec69d7537d90b422bbb
yukitake-9908 <yukibaseball.9908@gmail.com>
2024-09-07 16:19:16 +0900

    0907_74

--- msad2024_teamB/behaviorTreeCameraPID.py
+++ msad2024_teamB/behaviorTreeCameraPID.py
@@ -57,7 +57,8 @@ g_gyro_sensor: GyroSensor = None
 g_video: Video = None
 g_video_thread: threading.Thread = None
 g_course: int = 0
-g_dist: int = 1000
+global g_dist
+g_dist = 1000
 g_earned_dist: int = 0
 
 class TheEnd(Behaviour):
@@ -533,8 +534,6 @@ class IsDistanceEarned_after(Behaviour):
         self.delta_dist = delta_dist
         self.running = False
         self.earned = False
-        global g_dist
-        print(g_dist)
 
     def update(self) -> Status:
         if not self.running:
