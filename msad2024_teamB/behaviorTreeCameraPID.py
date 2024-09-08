65a85521ca2348c575c06de3920706e2186054a7
yukitake-9908 <yukibaseball.9908@gmail.com>
2024-09-08 16:57:59 +0900

    0908_132

--- msad2024_teamB/behaviorTreeCameraPID.py
+++ msad2024_teamB/behaviorTreeCameraPID.py
@@ -748,7 +748,7 @@ def build_behaviour_tree() -> BehaviourTree:
     )
     loop_10.add_children(
         [
-            MoveStraightLR(name="move straight 4", right_power=0, left_power=60, target_distance=100),
+            MoveStraightLR(name="move straight 4", right_power=0, left_power=60, target_distance=110),
         ]
     )
     loop_11.add_children(
