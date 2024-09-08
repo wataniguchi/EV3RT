9428a8a58d7807ede3bc29a2b78841c3fd048641
yukitake-9908 <yukibaseball.9908@gmail.com>
2024-09-08 09:38:30 +0900

    0908_5

--- msad2024_teamB/behaviorTreeCameraPID.py
+++ msad2024_teamB/behaviorTreeCameraPID.py
@@ -671,7 +671,7 @@ def build_behaviour_tree() -> BehaviourTree:
     )
     loop_01.add_children(
         [
-        TraceLineCam(name="trace normal edge", power=40, pid_p=1.0, pid_i=0.0015, pid_d=0.1,
+        TraceLineCam(name="trace normal edge", power=40, pid_p=0.5, pid_i=0.0015, pid_d=0.1,
                          gs_min=0, gs_max=80, trace_side=TraceSide.NORMAL),
         IsDistanceEarned_before(name="check distance", delta_dist = 1500),
         IsRedColorDetected(name="check red color", threshold=12.0), 
@@ -723,14 +723,14 @@ def build_behaviour_tree() -> BehaviourTree:
 
     loop_10.add_children(
         [
-            MoveStraightLR(name="move straight 4", right_power=0, left_power=60, target_distance=120),
+            MoveStraightLR(name="move straight 4", right_power=0, left_power=60, target_distance=100),
         ]
     )
 
     loop_11.add_children(
         [
-        TraceLineCam(name="trace normal edge", power=29, pid_p=1.0, pid_i=0.0015, pid_d=0.1,gs_min=0, gs_max=80, trace_side=TraceSide.NORMAL),
-        IsDistanceEarned(name="check distance", delta_dist=300),
+        TraceLineCam(name="trace normal edge", power=29, pid_p=0.5, pid_i=0.0015, pid_d=0.1,gs_min=0, gs_max=80, trace_side=TraceSide.NORMAL),
+        IsDistanceEarned(name="check distance", delta_dist=280),
         # TraceLineCam(name="trace normal edge", power=40, pid_p=1.0, pid_i=0.0015, pid_d=0.1,
         #                  gs_min=0, gs_max=80, trace_side=TraceSide.NORMAL),
         ]
@@ -744,12 +744,12 @@ def build_behaviour_tree() -> BehaviourTree:
 
     loop_13.add_children(
         [
-            MoveStraightLR(name="move straight 4", right_power=10, left_power=60, target_distance=250),
+            MoveStraightLR(name="move straight 4", right_power=0, left_power=60, target_distance=100),
         ]
     )
     loop_14.add_children(
         [
-        TraceLineCam(name="trace normal edge", power=40, pid_p=1.0, pid_i=0.0015, pid_d=0.1,
+        TraceLineCam(name="trace normal edge", power=40, pid_p=0.5, pid_i=0.0015, pid_d=0.1,
                          gs_min=0, gs_max=80, trace_side=TraceSide.NORMAL),
         IsDistanceEarned_before(name="check distance", delta_dist = 1500),
         IsRedColorDetected(name="check red color", threshold=12.0), 
