2cda37cc5c7a7b022a3a7936b169cf322923aa05
yukitake-9908 <yukibaseball.9908@gmail.com>
2024-09-09 21:27:27 +0900

    0909_72

--- msad2024_teamB/behaviorTreeCameraPID.py
+++ msad2024_teamB/behaviorTreeCameraPID.py
@@ -954,7 +954,7 @@ def build_behaviour_tree() -> BehaviourTree:
     # 指定距離走行
     loop_39.add_children(
         [
-        TraceLineCam(name="trace normal edge", power=38, pid_p=0.8, pid_i=0.0015, pid_d=0.1,
+        TraceLineCam(name="trace normal edge", power=40, pid_p=0.8, pid_i=0.0015, pid_d=0.1,
                          gs_min=0, gs_max=80, trace_side=TraceSide.NORMAL),
         IsDistanceEarned(name="check distance", delta_dist = 1050),
         ]
