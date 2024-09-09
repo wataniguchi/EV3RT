e04e8f1fd18fb1ad1e9c3b37fd092ee25090a586
ETrobo-B-sawai <mky3@docomo.ne.jp>
2024-09-09 19:36:21 +0900

    wloop_ms_v3_test3_20240909

--- msad2024_teamB/behaviorTreeCameraPID_test2_v3_wloop_4.py
+++ msad2024_teamB/behaviorTreeCameraPID_test2_v3_wloop_4.py
@@ -424,8 +424,11 @@ def build_behaviour_tree() -> BehaviourTree:
     #        pid_p=1.5→2.0→1,7→1.6→1.4→1.5
 
     #        TraceLineCam(name="trace normal edge", power=60, pid_p=1.2, pid_i=0.0015, pid_d=0.4, ok
+
+    #        TraceLineCam(name="trace normal edge", power=65, pid_p=0.5, pid_i=0.0015, pid_d=0.6,
+    #        pid_p=1.5→0.5  pid_d=0.4→0.6
                                                 
-            TraceLineCam(name="trace normal edge", power=65, pid_p=1.2, pid_i=0.0015, pid_d=0.4,
+            TraceLineCam(name="trace normal edge", power=65, pid_p=0.5, pid_i=0.0015, pid_d=0.6,
                          gs_min=0, gs_max=80, trace_side=TraceSide.NORMAL),
             IsDistanceEarned(name="check distance", delta_dist = 2100),
         ]
