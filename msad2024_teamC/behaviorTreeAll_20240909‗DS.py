6a165c880f8f7e25123218c261846c28acb74931
msad-kotarokitamura <175114307+msad-kotarokitamura@users.noreply.github.com>
2024-09-09 21:48:31 +0900

    const元に戻す

--- msad2024_teamC/behaviorTreeAll_20240909‗DS.py
+++ msad2024_teamC/behaviorTreeAll_20240909‗DS.py
@@ -865,8 +865,9 @@ def build_behaviour_tree() -> BehaviourTree:
     
     carry_01.add_children(
         [
-            RunAsInstructed(name="go straight",pwm_l=48,pwm_r=40),
-            IsDistanceEarned(name="check distance", delta_dist = 200),
+            RunAsInstructed(name="go straight",pwm_l=44,pwm_r=40),
+            #　IsDistanceEarned(name="check distance", delta_dist = 200),
+            IsDistanceEarned(name="check distance", delta_dist = 800),
         ]
     )
     
