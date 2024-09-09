69baca3ef315ae1441056c077b6d452143ffc263
msad-kotarokitamura <175114307+msad-kotarokitamura@users.noreply.github.com>
2024-09-09 21:14:45 +0900

    進入時のトレースをせんたーに変更

--- msad2024_teamC/behaviorTreeAll_20240909‗DS.py
+++ msad2024_teamC/behaviorTreeAll_20240909‗DS.py
@@ -872,7 +872,7 @@ def build_behaviour_tree() -> BehaviourTree:
     
     carry_02.add_children(
         [
-            TraceLineCam(name="trace normal edge", power=40, pid_p=1.5, pid_i=0.0015, pid_d=0.1,scene=Scene.DEBRI, gs_min=0, gs_max=80, trace_side=TraceSide.OPPOSITE),
+            TraceLineCam(name="trace normal edge", power=40, pid_p=1.5, pid_i=0.0015, pid_d=0.1,scene=Scene.DEBRI, gs_min=0, gs_max=80, trace_side=TraceSide.CENTER),
             IsDistanceEarned(name="check distance", delta_dist = 1000),
         ]
     )
