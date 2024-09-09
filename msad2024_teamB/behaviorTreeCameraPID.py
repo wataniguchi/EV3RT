23da4bdb277f149f8236c548b08d015e2f01c310
yukitake-9908 <yukibaseball.9908@gmail.com>
2024-09-09 21:08:09 +0900

    0909_62

--- msad2024_teamB/behaviorTreeCameraPID.py
+++ msad2024_teamB/behaviorTreeCameraPID.py
@@ -947,14 +947,14 @@ def build_behaviour_tree() -> BehaviourTree:
     # 右に90度回転
     loop_38.add_children(
         [
-            MoveStraightLR(name="move straight 4", right_power=0, left_power=60, target_distance=115),
+            MoveStraightLR(name="move straight 4", right_power=0, left_power=60, target_distance=105),
         ]
     )
 # 4列目
     # 指定距離走行_before、赤青判定
     loop_39.add_children(
         [
-        TraceLineCam(name="trace normal edge", power=32, pid_p=0.8, pid_i=0.0015, pid_d=0.1,
+        TraceLineCam(name="trace normal edge", power=38, pid_p=0.8, pid_i=0.0015, pid_d=0.1,
                          gs_min=0, gs_max=80, trace_side=TraceSide.OPPOSITE),
         IsDistanceEarned(name="check distance", delta_dist = 1050),
         ]
