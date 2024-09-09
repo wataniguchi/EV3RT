ece66851ebb70203c5a88b2e438a92f49ebe0735
yukitake-9908 <yukibaseball.9908@gmail.com>
2024-09-09 20:50:37 +0900

    0909_59

--- msad2024_teamB/behaviorTreeCameraPID.py
+++ msad2024_teamB/behaviorTreeCameraPID.py
@@ -704,8 +704,8 @@ def build_behaviour_tree() -> BehaviourTree:
     # 指定距離走行_before、赤青判定
     loop_01.add_children(
         [
-        TraceLineCam(name="trace normal edge", power=35, pid_p=1.2, pid_i=0.0015, pid_d=0.1,
-                         gs_min=0, gs_max=80, trace_side=TraceSide.NORMAL),
+        TraceLineCam(name="trace normal edge", power=35, pid_p=1.0, pid_i=0.0015, pid_d=0.1,
+                         gs_min=0, gs_max=80, trace_side=TraceSide.OPPOSITE),
         IsDistanceEarned_before(name="check distance", delta_dist = 1600),
         # IsRedColorDetected(name="check red color", threshold=12.0), 
         IsBlueColorDetected(name="check blue color", threshold=12.0), 
