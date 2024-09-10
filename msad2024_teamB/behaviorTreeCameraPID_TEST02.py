c07ba2accd86f7128dbd92121a04e8e77128e08a
ShiryuK <shiryu2000note@yahoo.co.jp>
2024-09-10 20:48:19 +0900

    0910_11

--- msad2024_teamB/behaviorTreeCameraPID_TEST02.py
+++ msad2024_teamB/behaviorTreeCameraPID_TEST02.py
@@ -426,7 +426,7 @@ def build_behaviour_tree() -> BehaviourTree:
     #カーブ1つ目
     loop_01_2.add_children(
         [
-            TraceLineCam(name="trace normal edge", power=55, pid_p=1.5, pid_i=0.0015, pid_d=0.3,
+            TraceLineCam(name="trace normal edge", power=50, pid_p=1.5, pid_i=0.0015, pid_d=0.3,
                          gs_min=0, gs_max=80, trace_side=TraceSide.NORMAL),
             IsDistanceEarned(name="check distance", delta_dist = 2000),
         ]
@@ -444,7 +444,7 @@ def build_behaviour_tree() -> BehaviourTree:
     #コンタクトⅠ直前
     loop_02.add_children(
         [
-            TraceLineCam(name="trace normal edge", power=50, pid_p=1.5, pid_i=0.001, pid_d=0.30,
+            TraceLineCam(name="trace normal edge", power=48, pid_p=1.5, pid_i=0.001, pid_d=0.30,
                          gs_min=0, gs_max=80, trace_side=TraceSide.NORMAL),
            IsDistanceEarned(name="check distance", delta_dist = 800),
 
@@ -464,7 +464,7 @@ def build_behaviour_tree() -> BehaviourTree:
     #コンタクトⅠ通過後、コンタクトⅡまで
     loop_03.add_children(
         [
-            TraceLineCam(name="trace opposite edge", power=45, pid_p=2.0, pid_i=0.0011, pid_d=0.35,
+            TraceLineCam(name="trace opposite edge", power=43, pid_p=2.0, pid_i=0.0011, pid_d=0.35,
                          gs_min=0, gs_max=80, trace_side=TraceSide.OPPOSITE),
             IsJunction(name="scan joined junction", target_state = JState.JOINED),
         ]
@@ -482,7 +482,7 @@ def build_behaviour_tree() -> BehaviourTree:
     #コンタクトⅢからコンタクトⅡまで
     loop_05.add_children(
         [
-            TraceLineCam(name="trace normal edge", power=45, pid_p=2.0, pid_i=0.0011, pid_d=0.35,
+            TraceLineCam(name="trace normal edge", power=44, pid_p=2.0, pid_i=0.0011, pid_d=0.35,
                          gs_min=0, gs_max=80, trace_side=TraceSide.NORMAL),
             IsJunction(name="scan joined junction", target_state = JState.JOINED),
         ]
