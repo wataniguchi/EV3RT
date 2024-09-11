ab1d0d12bac8640e5850c00bd338f21a9a54e207
KOTORINEKO <research.for.the.a@gmail.com>
2024-09-11 21:15:32 +0900

    20240911

--- msad2024_teamB/behaviorTree_SCarry_AN2.py
+++ msad2024_teamB/behaviorTree_SCarry_AN2.py
@@ -618,8 +618,7 @@ def build_behaviour_tree() -> BehaviourTree:
     step_01A_4.add_children(
         [
             TraceLineCam(name="trace buleline4", power=40, pid_p=1.0, pid_i=0.0015, pid_d=0.5,
-                 gs_min=0, gs_max=80, trace_side=TraceSide.NORMAL),
-                #  gs_min=0, gs_max=80, trace_side=TraceSide.CENTER),
+                 gs_min=0, gs_max=80, trace_side=TraceSide.CENTER),
             #IsDistanceEarned(name="check distance 1", delta_dist = 200),
             #Bottlecatch(name="trace CATCHED", target_state = BState.CATCHED),
             #Bottlecatch(name="linetrace", target_state = BState.LINE)
