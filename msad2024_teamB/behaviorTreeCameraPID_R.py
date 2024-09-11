50551c5d8e771100198b9473a18db42a4e1c956b
yukitake-9908 <yukibaseball.9908@gmail.com>
2024-09-11 18:50:36 +0900

    0911_5

--- msad2024_teamB/behaviorTreeCameraPID_R.py
+++ msad2024_teamB/behaviorTreeCameraPID_R.py
@@ -595,7 +595,7 @@ class IsDistanceEarned_before(Behaviour):
                 self.logger.info("%+06d %s.delta distance earned" % (cur_dist, self.__class__.__name__))
             global g_distFlg
             g_distFlg = True
-            g_dist = 1000
+            g_dist = 1300
             self.logger.info("グローバル変数更新 g_dist:980、g_earned_dist:0")
             return Status.SUCCESS
         else:
@@ -627,7 +627,7 @@ class IsDistanceEarned_after(Behaviour):
             if not self.earned:
                 self.earned = True
                 self.logger.info("%+06d %s.delta distance earned" % (cur_dist, self.__class__.__name__))
-            g_dist = 1000
+            g_dist = 1300
             g_earned_dist = 0
             self.logger.info("グローバル変数更新 g_dist:980、g_earned_dist:0")
             return Status.SUCCESS
