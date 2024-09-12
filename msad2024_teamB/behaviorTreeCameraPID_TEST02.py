b02dd3696602fb5de330bbed9d5a9e4d5c9d8f17
ShiryuK <shiryu2000note@yahoo.co.jp>
2024-09-12 20:50:58 +0900

    0912_5

--- msad2024_teamB/behaviorTreeCameraPID_TEST02.py
+++ msad2024_teamB/behaviorTreeCameraPID_TEST02.py
@@ -400,7 +400,7 @@ def build_behaviour_tree() -> BehaviourTree:
     #最初のストレート最高速度
     wloop_01.add_children(
         [
-            TraceLineCam(name="trace normal edge", power=60, pid_p=0.5, pid_i=0.0015, pid_d=0.6,
+            TraceLineCam(name="trace normal edge", power=60, pid_p=0.7, pid_i=0.0015, pid_d=0.6,
                          gs_min=0, gs_max=80, trace_side=TraceSide.NORMAL),
             IsDistanceEarned(name="check distance", delta_dist = 3500),
         ]
