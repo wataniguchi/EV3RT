65b7b3eddaf835d04f41eff185e3e8dd9a81cfca
yukitake-9908 <yukibaseball.9908@gmail.com>
2024-09-12 20:25:39 +0900

    0912_2

--- msad2024_teamB/behaviorTree_Merge_R.py
+++ msad2024_teamB/behaviorTree_Merge_R.py
@@ -868,7 +868,7 @@ def build_behaviour_tree() -> BehaviourTree:
     )
     dbr_loop_12.add_children(
         [
-            MoveStraight_dbr(name="move straight", power=40, target_distance=110),
+            MoveStraight_dbr(name="move straight", power=40, target_distance=120),
         ]
     )
     dbr_loop_13.add_children(
@@ -888,7 +888,7 @@ def build_behaviour_tree() -> BehaviourTree:
     )
     dbr_loop_16.add_children(
         [
-            MoveStraight_dbr(name="back", power=-50, target_distance=70)
+            MoveStraight_dbr(name="back", power=-50, target_distance=80)
         ]
     )
     # 指定距離走行_after
