c577c52bb1e27d7192848e50ce57ecf033eec83e
yukitake-9908 <yukibaseball.9908@gmail.com>
2024-09-12 19:11:45 +0900

    0912_merge3

--- msad2024_teamB/behaviorTree_Merge_L.py
+++ msad2024_teamB/behaviorTree_Merge_L.py
@@ -699,7 +699,7 @@ def build_behaviour_tree() -> BehaviourTree:
     dbr_loop_01.add_children(
 
         [
-        TraceLineCam(name="trace normal edge", power=39, pid_p=0.8, pid_i=0.0015, pid_d=0.1,
+        TraceLineCam(name="trace normal edge", power=38, pid_p=0.8, pid_i=0.0015, pid_d=0.1,
                          gs_min=0, gs_max=80, trace_side=TraceSide.OPPOSITE),
         IsDistanceEarned_before(name="check distance", delta_dist = 1600),
         IsRedColorDetected(name="check red color", threshold=12.0), 
