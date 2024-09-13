8cf7651bdccf05506ce5e9de4f391ddf7faa0e3c
KOTORINEKO <research.for.the.a@gmail.com>
2024-09-13 15:59:11 +0900

    20240913

--- msad2024_teamB/behaviorTree_SCarry_AN2.py
+++ msad2024_teamB/behaviorTree_SCarry_AN2.py
@@ -726,7 +726,7 @@ def build_behaviour_tree() -> BehaviourTree:
 
     step_03B_3.add_children(
         [
-            MoveStraightLR(name="Turn 3", right_power=40, left_power=0, target_distance=100),
+            MoveStraightLR(name="Turn 3", right_power=40, left_power=0, target_distance=150),
             # MoveStraightLR(name="back 2", right_power=-40, left_power=-30, target_distance=50),
         ]
     )
