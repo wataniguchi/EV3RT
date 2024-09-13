95442cf768771d37b6c1383ab8865fe387f166bc
Roy <saya22ih0734@gmail.com>
2024-09-13 19:26:37 +0900

    add

--- msad2024_teamB/behaviorTree_SCarry_AN2_right_Y.py
+++ msad2024_teamB/behaviorTree_SCarry_AN2_right_Y.py
@@ -769,7 +769,7 @@ def build_behaviour_tree() -> BehaviourTree:
     )
     test.add_children(
         [
-            CheckMotorAngles(name="a",right_power = 60, left_power = 0, total_ang_r = 360, total_ang_l = 0)
+            CheckMotorAngles(name="a",right_power = 60, left_power = 0, total_ang_r = 0, total_ang_l = 360)
         ]
     )
     
