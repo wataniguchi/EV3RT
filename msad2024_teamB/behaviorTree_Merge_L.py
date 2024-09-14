35bb91f8f30e46e804d593d84c580c4d3e0f4328
yukitake-9908 <yukibaseball.9908@gmail.com>
2024-09-14 12:19:05 +0900

    add

--- msad2024_teamB/behaviorTree_Merge_L.py
+++ msad2024_teamB/behaviorTree_Merge_L.py
@@ -883,7 +883,7 @@ def build_behaviour_tree() -> BehaviourTree:
     # 右に90度回転
     dbr_loop_10.add_children(
         [
-            MoveStraightLR(name="move straight 4", right_power=0, left_power=60, target_distance=115),
+            MoveStraightLR(name="move straight 4", right_power=0, left_power=60, target_distance=110),
         ]
     )
     # 指定距離走行_1列目から2列目移動
@@ -896,7 +896,7 @@ def build_behaviour_tree() -> BehaviourTree:
     # 右に90度回転
     dbr_loop_12.add_children(
         [
-            MoveStraightLR(name="move straight 4", right_power=0, left_power=60, target_distance=115),
+            MoveStraightLR(name="move straight 4", right_power=0, left_power=60, target_distance=110),
         ]
     )
 # 2列目
