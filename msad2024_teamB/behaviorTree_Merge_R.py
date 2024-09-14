2b0de9116721c541b8b5c55bf1d115c8d5a0bd3b
yukitake-9908 <yukibaseball.9908@gmail.com>
2024-09-14 11:36:44 +0900

    add

--- msad2024_teamB/behaviorTree_Merge_R.py
+++ msad2024_teamB/behaviorTree_Merge_R.py
@@ -910,7 +910,7 @@ def build_behaviour_tree() -> BehaviourTree:
     dbr_loop_19.add_children(
         [
         TraceLineCam(name="trace normal edge", power=35, pid_p=1.0, pid_i=0.0015, pid_d=0.1,gs_min=0, gs_max=80, trace_side=TraceSide.CENTER),
-        IsRedColorDetected(name="check red color", threshold=20.0), 
+        IsRedColorDetected(name="check red color", threshold=18.0), 
         ]
     )
     # デブリからボトル取得
