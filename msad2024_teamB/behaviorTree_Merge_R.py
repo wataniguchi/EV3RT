82be85383733f2903b1b1c7bb8c946ebe2b47cd8
yukitake-9908 <yukibaseball.9908@gmail.com>
2024-09-14 10:45:17 +0900

    add

--- msad2024_teamB/behaviorTree_Merge_R.py
+++ msad2024_teamB/behaviorTree_Merge_R.py
@@ -894,7 +894,7 @@ def build_behaviour_tree() -> BehaviourTree:
     # 指定距離走行_after
     dbr_loop_17.add_children(
         [
-        TraceLineCam(name="trace normal edge", power=35, pid_p=1.0, pid_i=0.0015, pid_d=0.1,gs_min=0, gs_max=80, trace_side=TraceSide.CENTER),
+        TraceLineCam(name="trace normal edge", power=35, pid_p=1.0, pid_i=0.0015, pid_d=0.1,gs_min=0, gs_max=80, trace_side=TraceSide.OPPOSITE),
         IsDistanceEarned_after(name="check distance"),
         ]
     )
