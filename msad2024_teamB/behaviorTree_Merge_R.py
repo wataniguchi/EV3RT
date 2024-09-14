0076dd437555cac740dd7208045fa74091ad3263
yukitake-9908 <yukibaseball.9908@gmail.com>
2024-09-14 11:52:04 +0900

    add

--- msad2024_teamB/behaviorTree_Merge_R.py
+++ msad2024_teamB/behaviorTree_Merge_R.py
@@ -902,14 +902,14 @@ def build_behaviour_tree() -> BehaviourTree:
     # 指定距離走行
     dbr_loop_18.add_children(
         [
-        TraceLineCam(name="trace normal edge", power=35, pid_p=1.0, pid_i=0.0015, pid_d=0.1,gs_min=0, gs_max=80, trace_side=TraceSide.CENTER),
+        TraceLineCam(name="trace normal edge", power=35, pid_p=1.0, pid_i=0.0015, pid_d=0.1,gs_min=0, gs_max=80, trace_side=TraceSide.NORMAL),
         IsDistanceEarned(name="check distance", delta_dist=255),
         ]
     )
     #スマートキャリーのボトルまで
     dbr_loop_19.add_children(
         [
-        TraceLineCam(name="trace normal edge", power=34, pid_p=1.0, pid_i=0.0015, pid_d=0.5,gs_min=0, gs_max=80, trace_side=TraceSide.CENTER),
+        TraceLineCam(name="trace normal edge", power=34, pid_p=1.0, pid_i=0.0015, pid_d=0.5,gs_min=0, gs_max=80, trace_side=TraceSide.NORMAL),
         IsRedColorDetected(name="check red color", threshold=18.0), 
         ]
     )
