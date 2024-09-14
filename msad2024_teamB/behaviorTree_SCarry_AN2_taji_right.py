73a12b26b6fa09dd6a0508c95902dce457da06a3
mt-robo <kait8tajiri5m@gmail.com>
2024-09-14 16:20:07 +0900

    add

--- msad2024_teamB/behaviorTree_SCarry_AN2_taji_right.py
+++ msad2024_teamB/behaviorTree_SCarry_AN2_taji_right.py
@@ -680,7 +680,7 @@ def build_behaviour_tree() -> BehaviourTree:
             #IsDistanceEarned(name="check distance 1", delta_dist = 200),
             #Bottlecatch(name="trace CATCHED", target_state = BState.CATCHED),
             #Bottlecatch(name="linetrace", target_state = BState.LINE)
-            IsRedColorDetected(name="red", threshold=4),
+            IsRedColorDetected(name="red", threshold=12),
             #IsDistanceEarned(name="check distance 1", delta_dist = 500),
         ]
     )
