e95b45f3871fa9f36aed7708b04ab6a221f853e7
yukitake-9908 <yukibaseball.9908@gmail.com>
2024-09-14 18:59:27 +0900

    add

--- msad2024_teamB/behaviorTree_Merge_L.py
+++ msad2024_teamB/behaviorTree_Merge_L.py
@@ -767,7 +767,7 @@ def build_behaviour_tree() -> BehaviourTree:
     step_02B = Sequence(name="step 02B", memory=True)
     step_03B_1 = Sequence(name="step 03B_1", memory=True)
     step_03B_2 = Parallel(name="step 03B_2", policy=ParallelPolicy.SuccessOnOne())
-    step_03B_3 = Sequence(name="step 03B_3", memory=False)
+    step_03B_3 = Sequence(name="step 03B_3", memory=True)
     step_04B = Parallel(name="step 04B", policy=ParallelPolicy.SuccessOnOne())
     step_04B_2 = Parallel(name="step 04B", policy=ParallelPolicy.SuccessOnOne())
 
