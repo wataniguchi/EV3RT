2142f53d0a1ec08fc3ce1a92c94d9b614aedfecf
yukitake-9908 <yukibaseball.9908@gmail.com>
2024-09-14 19:05:45 +0900

    add

--- msad2024_teamB/behaviorTree_Merge_L.py
+++ msad2024_teamB/behaviorTree_Merge_L.py
@@ -1180,7 +1180,7 @@ def build_behaviour_tree() -> BehaviourTree:
         [
             TraceLineCam(name="trace buleline4", power=40, pid_p=1.0, pid_i=0.0015, pid_d=0.5,
                  gs_min=0, gs_max=80, trace_side=TraceSide.CENTER),
-            IsDistanceEarned(name="check distance 1", delta_dist = 400),
+            IsDistanceEarned(name="check distance 1", delta_dist = 380),
         ]
     )
     # ボトル取得からサークルへ配置
@@ -1218,7 +1218,8 @@ def build_behaviour_tree() -> BehaviourTree:
         [
             TraceLineCam(name="last run", power=40, pid_p=2.5, pid_i=0.0015, pid_d=0.1,
                          gs_min=0, gs_max=80, trace_side=TraceSide.CENTER),
-            IsDistanceEarned(name="check distance 2", delta_dist = 500),
+            IsDistanceEarned(name="check distance 2", delta_dist = 850),
+            IsBlueColorDetected(name="check blue color", threshold=4.0),
         ]
     )
 
