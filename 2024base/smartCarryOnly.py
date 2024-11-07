8f7a324cd0aed91fc0eb23c8252338b1f246458d
msad-kotarokitamura <175114307+msad-kotarokitamura@users.noreply.github.com>
2024-11-07 20:56:06 +0900

    13241

--- 2024base/smartCarryOnly.py
+++ 2024base/smartCarryOnly.py
@@ -982,7 +982,7 @@ def build_behaviour_tree() -> BehaviourTree:
     carry_04.add_children(
         [
             RunAsInstructed(name="rotate", pwm_r=35,pwm_l=-50),
-            IsRotated(name="check rotated", delta_dire=76),
+            IsRotated(name="check rotated", delta_dire=73),
         ]
     )
 
@@ -990,7 +990,7 @@ def build_behaviour_tree() -> BehaviourTree:
     carry_05.add_children(
         [
             RunAsInstructed(name="go straight",pwm_l=50,pwm_r=50),
-            IsDistanceEarned(name="check distance", delta_dist = 1460),
+            IsDistanceEarned(name="check distance", delta_dist = 1450),
         ]
     )
     
